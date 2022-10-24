7db6a6b811372f701ea15ffbfa8170edc5ca4e3c
LiuSheng <liusheng2@tal.com>
2022-10-24 11:23:31 +0800

    init
    初始化环境

---
+++ 2021104413/src/test/java/edu/ruc/liu/elkdemo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ruc.liu.elkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
